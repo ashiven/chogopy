345e5f5143833ff2df11d540946a94ee81567113
ashiven <nevisha@pm.me>
2025-08-15 16:36:02 +0200

    start implementing consume token

--- pkg/lexer/tokenizer.go
+++ pkg/lexer/tokenizer.go
@@ -22,16 +22,32 @@ func NewTokenizer(scanner Scanner) Tokenizer {
 
 func (t *Tokenizer) Peek(tokenAmount int) []Token {
 	if len(t.tokenBuffer) == 0 {
-		t.tokenBuffer = append(t.tokenBuffer, t.Consume())
+		t.tokenBuffer = append(t.tokenBuffer, t.Consume(false))
 	}
 
 	for len(t.tokenBuffer) < tokenAmount {
-		t.tokenBuffer = append(t.tokenBuffer, t.Consume())
+		t.tokenBuffer = append(t.tokenBuffer, t.Consume(true))
 	}
 
 	return t.tokenBuffer[:tokenAmount]
 }
 
-func (t *Tokenizer) Consume() Token {
+func (t *Tokenizer) Consume(keepBuffer bool) Token {
+	// :param keepBuffer:
+	// This is only useful for when we want to look ahead for more than one token. (Inside of Tokenizer.Peek())
+	// In that case, only the first token is actually consumed and the rest of the tokens are only inspected.
+	if len(t.tokenBuffer) > 0 && !keepBuffer {
+		token := t.tokenBuffer[0]
+		t.tokenBuffer = t.tokenBuffer[1:]
+		return token
+	}
+
+	nextChar := t.scanner.Consume()
+	_ = nextChar
+
+	for {
+		break
+	}
+
 	return Token{}
 }
