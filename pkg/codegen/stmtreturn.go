1479c95f56c543015cf6f7de37bac80fad0560d9
ashiven <nevisha@pm.me>
2025-08-29 14:33:36 +0200

    fix return load

--- pkg/codegen/stmtreturn.go
+++ pkg/codegen/stmtreturn.go
@@ -8,9 +8,14 @@ import (
 
 func (cg *CodeGenerator) VisitReturnStmt(returnStmt *ast.ReturnStmt) {
 	var returnVal value.Value
+
 	if returnStmt.ReturnVal != nil {
 		returnStmt.ReturnVal.Visit(cg)
 		returnVal = cg.lastGenerated
+
+		if isIdentOrIndex(returnStmt.ReturnVal) {
+			returnVal = cg.LoadVal(returnVal)
+		}
 	}
 
 	cg.currentBlock.NewRet(returnVal)
