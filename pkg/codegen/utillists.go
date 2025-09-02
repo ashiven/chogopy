e3226b80a77759b38b0080d2a2ca162f14deabcb
ashiven <nevisha@pm.me>
2025-09-02 07:34:58 +0200

    implement list concatenation

--- pkg/codegen/utillists.go
+++ pkg/codegen/utillists.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/llir/llvm/ir"
 	"github.com/llir/llvm/ir/constant"
 	"github.com/llir/llvm/ir/types"
 	"github.com/llir/llvm/ir/value"
@@ -53,7 +52,7 @@ func (cg *CodeGenerator) getListElem(list value.Value, index value.Value) value.
 	return listElem
 }
 
-func (cg *CodeGenerator) setLen(list value.Value, len value.Value) {
+func (cg *CodeGenerator) setListLen(list value.Value, len value.Value) {
 	zero := constant.NewInt(types.I32, 0)
 	one := constant.NewInt(types.I32, 1)
 	listLenAddr := cg.currentBlock.NewGetElementPtr(list.Type().(*types.PointerType).ElemType, list, zero, one)
@@ -61,7 +60,7 @@ func (cg *CodeGenerator) setLen(list value.Value, len value.Value) {
 	cg.NewStore(len, listLenAddr)
 }
 
-func (cg *CodeGenerator) setInit(list value.Value, init value.Value) {
+func (cg *CodeGenerator) setListInit(list value.Value, init value.Value) {
 	zero := constant.NewInt(types.I32, 0)
 	two := constant.NewInt(types.I32, 2)
 	listInitAddr := cg.currentBlock.NewGetElementPtr(list.Type().(*types.PointerType).ElemType, list, zero, two)
@@ -69,7 +68,7 @@ func (cg *CodeGenerator) setInit(list value.Value, init value.Value) {
 	cg.NewStore(init, listInitAddr)
 }
 
-func (cg *CodeGenerator) setContent(list value.Value, content value.Value) {
+func (cg *CodeGenerator) setListContent(list value.Value, content value.Value) {
 	zero := constant.NewInt(types.I32, 0)
 	listContentAddr := cg.currentBlock.NewGetElementPtr(list.Type().(*types.PointerType).ElemType, list, zero, zero)
 	listContentAddr.LocalName = cg.uniqueNames.get("list_content_addr")
@@ -78,30 +77,41 @@ func (cg *CodeGenerator) setContent(list value.Value, content value.Value) {
 
 func (cg *CodeGenerator) concatLists(lhs value.Value, rhs value.Value, listType types.Type) value.Value {
 	zero := constant.NewInt(types.I32, 0)
+	four := constant.NewInt(types.I32, 4)
+
 	lhsContentPtr := cg.getListElemPtr(lhs, zero)
 	lhsLen := cg.currentBlock.NewCall(cg.functions["listlen"], lhs)
+	// TODO: We are multiplying the list lengths by four because memcpy expects a length in bytes (i8) rather than words (i32).
+	// However, if we are concatenating nested lists, we may need to adjust these lengths differently.
+	lhsLenByte := cg.currentBlock.NewMul(lhsLen, four)
 
 	rhsContentPtr := cg.getListElemPtr(rhs, zero)
 	rhsLen := cg.currentBlock.NewCall(cg.functions["listlen"], rhs)
+	rhsLenByte := cg.currentBlock.NewMul(rhsLen, four)
 
+	concatPtr := cg.currentBlock.NewAlloca(listType)
 	concatLen := cg.currentBlock.NewAdd(lhsLen, rhsLen)
 	concatInit := constant.NewBool(true)
 
-	concatPtr := cg.currentBlock.NewAlloca(listType)
 	concatListElemType := getListElemTypeFromListType(listType)
-
-	// TODO: have to allocate enough for the actual size of the concatenated list (currently only allocates for one element)
 	concatContentPtr := cg.NewAllocN(concatListElemType, concatLen)
-	cg.currentBlock.NewCall(cg.functions["memcpy"], concatContentPtr, lhsContentPtr, lhsLen)
+	cg.currentBlock.NewCall(cg.functions["memcpy"], concatContentPtr, lhsContentPtr, lhsLenByte)
 
-	// TODO: adjust for nested lists
-	concatContentPtrShifted := cg.currentBlock.NewGetElementPtr(concatContentPtr.ElemType, concatContentPtr, lhsLen)
-	cg.currentBlock.NewCall(cg.functions["memcpy"], concatContentPtrShifted, rhsContentPtr, rhsLen)
+	if isList(concatContentPtr) {
+		/* Content is another list */
+		contentIdx := constant.NewInt(types.I32, 0)
+		concatContentPtrShifted := cg.currentBlock.NewGetElementPtr(concatContentPtr.ElemType, concatContentPtr, lhsLen, contentIdx)
+		cg.currentBlock.NewCall(cg.functions["memcpy"], concatContentPtrShifted, rhsContentPtr, rhsLenByte)
 
-	cg.setLen(concatPtr, concatLen)
-	cg.setInit(concatPtr, concatInit)
-	cg.setContent(concatPtr, concatContentPtr)
+	} else {
+		/* Regular list content */
+		concatContentPtrShifted := cg.currentBlock.NewGetElementPtr(concatContentPtr.ElemType, concatContentPtr, lhsLen)
+		cg.currentBlock.NewCall(cg.functions["memcpy"], concatContentPtrShifted, rhsContentPtr, rhsLenByte)
+	}
 
+	cg.setListLen(concatPtr, concatLen)
+	cg.setListInit(concatPtr, concatInit)
+	cg.setListContent(concatPtr, concatContentPtr)
 	return concatPtr
 }
 
@@ -121,20 +131,25 @@ func (cg *CodeGenerator) newList(listElems []value.Value, listType types.Type) v
 	/* list.content store */
 	for elemIdx, elem := range listElems {
 		elemIdx := constant.NewInt(types.I32, int64(elemIdx))
-		var elemAddr *ir.InstGetElementPtr
+
 		if isList(listContentPtr) {
+			/* Content is another list */
 			contentIdx := constant.NewInt(types.I32, 0)
-			elemAddr = cg.currentBlock.NewGetElementPtr(listElemType, listContentPtr, elemIdx, contentIdx)
+			elemAddr := cg.currentBlock.NewGetElementPtr(listElemType, listContentPtr, elemIdx, contentIdx)
+			elemAddr.LocalName = cg.uniqueNames.get("list_content_elem_addr")
+			cg.NewStore(elem, elemAddr)
+
 		} else {
-			elemAddr = cg.currentBlock.NewGetElementPtr(listElemType, listContentPtr, elemIdx)
+			/* Regular list content */
+			elemAddr := cg.currentBlock.NewGetElementPtr(listElemType, listContentPtr, elemIdx)
+			elemAddr.LocalName = cg.uniqueNames.get("list_content_elem_addr")
+			cg.NewStore(elem, elemAddr)
 		}
-		elemAddr.LocalName = cg.uniqueNames.get("list_content_elem_addr")
-		cg.NewStore(elem, elemAddr)
 	}
 
 	/* list store */
-	cg.setLen(listPtr, listLen)
-	cg.setInit(listPtr, listInit)
-	cg.setContent(listPtr, listContentPtr)
+	cg.setListLen(listPtr, listLen)
+	cg.setListInit(listPtr, listInit)
+	cg.setListContent(listPtr, listContentPtr)
 	return listPtr
 }
